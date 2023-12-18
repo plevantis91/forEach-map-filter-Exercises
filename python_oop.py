60cc6d21a121a8a8bc105336ee8a176d82a00255
plevantis91 <143660462+plevantis91@users.noreply.github.com>
2023-12-18 18:36:10 -0500

    Add files via upload

---
+++ python_oop.py
@@ -0,0 +1,81 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+    def __init__(self, start=0):
+        self.start = self.new 
+    
+    def generate(self):
+        self.new += 1
+        return self.new
+    
+    def reset(self):
+        self.new = self.start
+
+import random 
+
+class WordFinder:
+    """Machine for finding random words from dictionary.
+    
+    >>> wf = WordFinder("simple.txt")
+    3 words read
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+    """
+    def __init__(self,path):
+        open_file = open(path)
+
+        self.words =self.parse(open_file)
+
+    def parse(self,open_file):
+        return [w.strip() for w in open_file]
+    
+    def random(self):
+        return random.choice(self.words)
+    
+
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments.
+    
+    >>> swf = SpecialWordFinder("complex.txt")
+    3 words read
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+    """
+
+    def parse(self, dict_file):
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
